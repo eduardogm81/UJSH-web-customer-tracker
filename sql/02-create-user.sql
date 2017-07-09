6a99dee6bf0992b3fc32971289e3ace192874c18
eduardogm81 <eduardogm81@gmail.com>
2017-07-09 19:38:04 +0200

    Primera versión con un servlet para probar la conexión con base de datos

---
+++ sql/02-create-user.sql
@@ -0,0 +1,5 @@
+CREATE USER 'springstudent'@'localhost' IDENTIFIED BY 'springstudent';
+
+GRANT ALL PRIVILEGES ON web_customer_tracker.* TO 'springstudent'@'localhost';
+
+/* drop user 'springstudent'@'localhost'; */
